0383300086227f0fc0e5d86f6feacc683f474fbf
Renata <renataaupova83@gmail.com>
2021-03-22 01:41:11 +0300

    first commit

--- config.py
+++ config.py
@@ -7,10 +7,4 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     CSRF_ENABLED = True  # активирует предотвращение поддельных межсайтовых запросов
-    OPENID_PROVIDERS = [
-        {'name': 'Google', 'url': 'https://www.google.com/accounts'},
-        {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
-        {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
-        {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
-        {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
