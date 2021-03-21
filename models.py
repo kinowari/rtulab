85f99efd75f5a85748fe413c9b455f063ae92edf
renataa21 <56071777+renataa21@users.noreply.github.com>
2021-03-21 14:20:43 +0300

    Add files via upload

--- models.py
+++ models.py
@@ -8,22 +8,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
-    #checks = db.Column(db.String(120))
 
-    # def is_authenticated(self):
-    #     return True
-    #
-    # def is_active(self):
-    #     return True
-    #
-    # def is_anonymous(self):
-    #     return False
-    #
-    # def get_id(self):
-    #     try:
-    #         return str(self.id)
-    #     except:
-    #         NameError
 
     def __repr__(self):  # как выводить объекты этого класса.
         return '<User %r>' % (self.nickname)
@@ -37,7 +22,7 @@ class Check(db.Model):
     mount = db.Column(db.Integer, index=True)
     category = db.Column(db.String(64), index=True)
     payment_method = db.Column(db.SmallInteger)  # 0 наличка 1 карта
-    user_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, index=True)
 
 
 class Market(db.Model):
@@ -45,8 +30,16 @@ class Market(db.Model):
     name = db.Column(db.String(64), index=True, unique=True)
     address = db.Column(db.String(120), )
     phone_number = db.Column(db.Integer, index=True)
-    all_product = db.Column(db.String(555), index=True)
-    amount_product = db.Column(db.Integer)
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(256), index = True)
+    description = db.Column(db.String(64), index=True)
+    price = db.Column(db.Integer, index=True)
+    mount = db.Column(db.Integer, index=True)
+    category = db.Column(db.String(64), index=True)
+    id_market = db.Column(db.Integer, index=True)
 
 
 @lm.user_loader
--- views.py
+++ views.py
@@ -1,13 +1,14 @@
-from flask import render_template, flash, redirect, g, url_for, request
+from flask import render_template, flash, redirect, session, url_for, request
 from flask_login import  logout_user, current_user
 from app import app, db, lm
 from app.models import User, Check
 import time
 
+
 @app.route('/')
 @app.route('/index')
 def index():
-
+    session['nickname'] = ''
     user = current_user
     posts = [
         {
@@ -16,7 +17,7 @@ def index():
         },
         {
             'author': {'nickname': 'Renata2'},
-            'body': 'ну я пыталась (само собо писалось все с 0, было мого проблем)'
+            'body': 'ну я пыталась (само собой писалось все с 0, было много проблем)'
         }
     ]
     return render_template('index.html',
@@ -37,17 +38,19 @@ def login():
         if user is None:
             flash('Вы удачо зарегестрировались')
             print(nickname,email)
-            userr=User(nickname=nickname,email=email)
-            g.nickname = nickname
-            db.session.add(userr)
+            new_user=User(nickname=nickname,email=email)
+
+            session['nickname'] = nickname
+            db.session.add(new_user)
             db.session.commit()
-            return render_template('user.html', nickname=userr.nickname)
+            return render_template('user.html', nickname=new_user.nickname)
         elif email!= user.email:
             flash('Вы ввели неправильный email или nickname')
             return redirect(url_for("login"))
         else:
             flash('Добро пожаловать, '+ nickname)
-            g.nickname = nickname
+            session['nickname'] = nickname
+
             return render_template('user.html', nickname=nickname)
     else:return render_template("login.html")
 
@@ -57,7 +60,7 @@ def login():
 def logout():
 
     logout_user()
-    return redirect(url_for('index'))
+    return redirect(url_for('login'))
 
 
 @lm.user_loader
@@ -68,10 +71,12 @@ def load_user(id):
 
 @app.route('/user', methods = ['GET', 'POST'])
 def user():
-    user = User.query.filter_by(nickname='john').first()
+    nickname = session['nickname']
+    user = User.query.filter_by(nickname=nickname).first()
+
     if user == None:
         flash('User ' + ' not found.')
-        return redirect(url_for('index'))
+        return redirect(url_for('login'))
 
     return render_template('user.html',
                            user=user,
@@ -80,9 +85,12 @@ def user():
 @app.route('/check', methods = ['GET', 'POST'])
 def check():
 
+    nickname = session['nickname']
+
+    user = User.query.filter_by(nickname=nickname).first()
+    print(user.id)
     if request.method == "POST":
 
-        user = User.query.filter_by(nickname='john').first()
         times=time.asctime()
         product = request.form.get('product')
         price = request.form.get('price')
@@ -90,11 +98,8 @@ def check():
         category = request.form.get('category')
         payment_method = request.form.get('payment_method')
         print(times,price,product,mount,category,payment_method)
-        check = Check( times = times, product = product,price=price, mount=mount,category=category,payment_method=int(payment_method))
+        check = Check( times = str(times), product = product,price=price, mount=mount,category=category,payment_method=int(payment_method), user_id = user.id)
         db.session.add(check)
-        db.session.commit()
-        #user.checks=user.checks+str(check.id)
-        db.session.add()
         db.session.commit()
         if price == ''or product =='' or mount == '' or category == '' or payment_method == '' or payment_method == None:
             flash('Одно из полей осталось пустым')
@@ -107,20 +112,12 @@ def check():
 
 @app.route('/checks')
 def checks():
+    nickname = session['nickname']
+    user = User.query.filter_by(nickname=nickname).first()
+    checks= Check.query.filter_by(user_id = user.id).all()
 
-    user = User.query.filter_by(nickname= 'jon').first()
-    ids= user.checks
-    if user.checks is not None:
-
-        for i in len(user.checks):
-            check=Check.query.filter_by(id=i).first()
-            times = check.temes
-            product = check.product
-            price = check.price
-            mount = check.mount
-            category = check.category
-            payment_method = check.payment_method
-            return redirect(url_for('checks'))
+    if checks:
+        return render_template("checks.html",checks=checks)
     else:
         flash("Вы еще не создали чеков")
         return redirect(url_for('user'))
